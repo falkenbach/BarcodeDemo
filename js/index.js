f1a9a9653ef02ca59b23dcc52ff9599ba53f869e
Alexey Udovydchenko <alexey.udovydchenko@gmail.com>
2013-06-07 10:00:47 +0700

    Added website integration and encoding

--- js/index.js
+++ js/index.js
@@ -1,42 +1,14 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // `load`, `deviceready`, `offline`, and `online`.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.getElementById('scan').addEventListener('click', this.scan, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of `this` is the event. In order to call the `receivedEvent`
-    // function, we must explicity call `app.receivedEvent(...);`
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
-    // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
@@ -46,26 +18,104 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    },
-    scan: function() {
-        console.log('scanning');
-        try {
-            window.plugins.barcodeScanner.scan(function(args) {
-                console.log("Scanner result: \n" +
-                    "text: " + args.text + "\n" +
-                    "format: " + args.format + "\n" +
-                    "cancelled: " + args.cancelled + "\n");
-                /*
-                if (args.format == "QR_CODE") {
-                    window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
-                }
-                */
-                document.getElementById("info").innerHTML = args.text;
-                console.log(args);
-        });
-        } catch (ex) {
-            console.log(ex.message);
-        }
     }
 
+};
+
+var scanSuccess = function(result) {
+    var textFormats = "QR_CODE DATA_MATRIX";
+    var productFormats = "UPC_E UPC_A EAN_8 EAN_13";
+
+    if (result.cancelled) {
+        return;
+    }
+    //alert("Scanned Code: " + result.text + ". Format: " + result.format);
+    if (textFormats.match(result.format)) {
+        var scanVal = result.text;
+        if (scanVal.indexOf("http") === 0) {
+            navigator.notification.confirm(
+                    '(1) Search at Website\n(2) Cancel',
+                    function (b) {
+                        if (b === 1) {
+                            window.plugins.childBrowser.openExternal(scanVal);
+                        }
+                    },
+                    result.text,
+                    '1, 2'
+            );
+        } else {
+            navigator.notification.alert(
+                    result.text,
+                    function (){},
+                    'Scan Value:',
+                    'Done'
+                );
+        }
+    } else if (productFormats.match(result.format)) {
+        //alert("Found Product Code: " + result.text  + ". Format: " + result.format);
+        navigator.notification.confirm(
+                '(1) Look Up Product\n(2) Cancel',
+                function (b) {
+                    if (b === 1) {
+                        // This is an example url with query - substitute your own here
+                        var searchUrl = "http://ndd.ycom.no/Search?SearchText=" + result.text;
+                        window.plugins.childBrowser.showWebPage(searchUrl);
+                    }
+                },
+                result.text,
+                '1, 2'
+        );
+    } else {
+        alert("Scan format : " + result.format +
+              " not supported. Scan value: " + result.text);
+    }
+};
+
+var scanCode = function() {
+window.plugins.barcodeScanner.scan(
+        scanSuccess,
+        function(error) {
+            alert("Scan failed: " + error);
+        });
+};
+
+var encodeText = function() {
+    window.plugins.barcodeScanner.encode(
+            BarcodeScanner.Encode.TEXT_TYPE,
+            "http://www.ndd.ycom.no/",
+            function(success) {
+                alert("Encode success: " + success);
+            }, function(fail) {
+                alert("Encoding failed: " + fail);
+            });
+};
+
+var encodeEmail = function() {
+    window.plugins.barcodeScanner.encode(
+        BarcodeScanner.Encode.EMAIL_TYPE,
+        "admin@ndd.ycom.no", function(success) {
+            alert("Encode success: " + success);
+        }, function(fail) {
+            alert("Encoding failed: " + fail);
+        });
+};
+
+var encodePhone = function() {
+    window.plugins.barcodeScanner.encode(
+        BarcodeScanner.Encode.PHONE_TYPE,
+        "66-12345678", function(success) {
+            alert("Encode success: " + success);
+        }, function(fail) {
+            alert("Encoding failed: " + fail);
+        });
+};
+
+var encodeSMS = function() {
+    window.plugins.barcodeScanner.encode(
+        BarcodeScanner.Encode.SMS_TYPE,
+        "An important message for someone.", function(success) {
+            alert("Encode success: " + success);
+        }, function(fail) {
+            alert("Encoding failed: " + fail);
+        });
 };
